08e001f478571a2598eaff9dd5b92ccda92674c6
99yosr <96025405+99yosr@users.noreply.github.com>
2025-12-02 13:44:16 +0100

    Change PostgreSQL database connection URL
    Updated database connection URL for PostgreSQL.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://rdsadmin123:azertyuiop123@database-1.chok2eusm4x9.us-east-1.rds.amazonaws.com/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@database-examen.chok2eusm4x9.us-east-1.rds.amazonaws.com/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
